73055e4567d8520ba441f18b2b3da1f81d5d621f
pavan4610 <knvd.pavan@gmail.com~>
2022-06-12 18:29:16 +0530

    first commit

---
+++ src/main/java/com/example/devopsintegarion/DevopsintegarionApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsintegarion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsintegarionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsintegarionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsintegarion/DevopsintegarionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsintegarion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsintegarionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
